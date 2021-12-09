c531fb44b615901cfdd148ef1e1f0b73609aee42
Laura Kutschan <laurakutschan@Lauras-MacBook-Pro.local>
2021-12-09 16:31:19 +0100

    Task status update

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -19,30 +19,51 @@ export class TodoComponent implements OnInit {
 
   task: Tasks = { goalid: '', _id: '', description: '', status: ''};
   description = '';
-
   status = '';
-
   showData: boolean = false;
 
   constructor(public dialog: MatDialog, private router: Router, private api: ApiService, private route: ActivatedRoute,) { }
 
   ngOnInit(): void {
-
   }
 
-  public changeStatus(): void {
-        this.tasksToDoing.forEach((task: Tasks) => {
-          console.log(task._id);
-          task.status = String('doing');
-          this.api.updateTaskStatus(task._id, task).subscribe((task: Tasks) => {
-          }, error => {
-            console.log('hat nicht funktioniert');
-          });
-      });
+    fillArrays() {
+    console.log("vorher " + this.tasksToOneGoal);
+    console.log("Doing " + this.tasksToDoing);
+    for(let task of this.tasksToOneGoal) {
+      if (task.status == "doing") {
+        let index = this.tasksToOneGoal.indexOf(task);
+        this.tasksToOneGoal.splice(index,1);
+        this.tasksToDoing.push(task);
+      } else if(task.status == "done") {
+        let index = this.tasksToOneGoal.indexOf(task);
+        this.tasksToOneGoal.splice(index,1);
+        this.tasksToDone.push(task);
+      }
+    }
+    console.log("nachher " + this.tasksToOneGoal);
+    console.log("Doing nachher " + this.tasksToDoing);
+    this.ngOnInit();
+  }
+
+  /*TODO*/
+
+  public changeStatusToTodo(): void {
+    this.tasksToOneGoal.forEach((task: Tasks) => {
+      if(task.status != 'todo') {
+        console.log(task._id);
+        task.status = String('todo');
+        this.api.updateTaskStatus(task._id, task).subscribe((task: Tasks) => {
+        }, error => {
+          console.log('hat nicht funktioniert');
+        });
+      }
+    });
   }
 
 
-  drop(event: CdkDragDrop<any>) {
+  dropInTodo(event: CdkDragDrop<any>) {
+    console.log('vorher ' + this.tasksToOneGoal);
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
       console.log('moveIteminArray aufgerufen');
@@ -53,11 +74,81 @@ export class TodoComponent implements OnInit {
         event.previousIndex,
         event.currentIndex,
       );
-
-      console.log('transferArrayItem aufgerufen');
-      this.changeStatus();
+      console.log('nachher ' + this.tasksToOneGoal);
+      this.changeStatusToTodo();
     }
   }
 
 
+
+
+  /*DOING*/
+
+  public changeStatusToDoing(): void {
+        this.tasksToDoing.forEach((task: Tasks) => {
+          if(task.status != 'doing') {
+            console.log(task._id);
+            task.status = String('doing');
+            this.api.updateTaskStatus(task._id, task).subscribe((task: Tasks) => {
+            }, error => {
+              console.log('hat nicht funktioniert');
+            });
+          }
+      });
+  }
+
+
+  dropInDoing(event: CdkDragDrop<any>) {
+    console.log('vorher ' + this.tasksToOneGoal);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      console.log('moveIteminArray aufgerufen');
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+      console.log('nachher ' + this.tasksToOneGoal);
+      this.changeStatusToDoing();
+    }
+  }
+
+
+
+
+
+  /*DONE*/
+
+  public changeStatusToDone(): void {
+    this.tasksToDone.forEach((task: Tasks) => {
+      if(task.status != 'done') {
+        console.log(task._id);
+        task.status = String('done');
+        this.api.updateTaskStatus(task._id, task).subscribe((task: Tasks) => {
+        }, error => {
+          console.log('hat nicht funktioniert');
+        });
+      }
+    });
+  }
+
+
+  dropInDone(event: CdkDragDrop<any>) {
+    console.log('vorher ' + this.tasksToOneGoal);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      console.log('moveIteminArray aufgerufen');
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+      console.log('nachher ' + this.tasksToOneGoal);
+      this.changeStatusToDone();
+    }
+  }
 }
